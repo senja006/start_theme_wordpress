3baee98fa076e4798e8ff47a9d7764ee9a5fdd02
Sergey Yarkevich <senja999@mail.ru>
2017-01-01 17:23:23 +0300

    правки

--- functions.php
+++ functions.php
@@ -544,3 +544,29 @@ function get_file_size( $id ) {
 
     return size_format( $filesize, 2 );
 }
+
+
+/**
+ * Получение атрибутов шорткода из текста в виде массива
+ *
+ * @uses get_shortcode_regex()
+ * @uses shortcode_parse_atts()
+ * @param  string $tag   Shortcode tag
+ * @param  string $text  Text containing shortcodes
+ * @return array  $out   Array of attributes
+ */
+
+function get_all_shortcode_attributes( $tag, $text )
+{
+    preg_match_all( '/' . get_shortcode_regex() . '/s', $text, $matches );
+    $out = array();
+    if( isset( $matches[2] ) )
+    {
+        foreach( (array) $matches[2] as $key => $value )
+        {
+            if( $tag === $value )
+                $out[] = shortcode_parse_atts( $matches[3][$key] );
+        }
+    }
+    return $out;
+}
